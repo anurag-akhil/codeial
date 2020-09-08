e729457b0d018962f46abf1866ee137f109f5437
akhil <akhil.anurag1999@gmail.com>
2020-09-08 17:36:00 +0530

    add manual authentication

--- controller/user_profile.js
+++ controller/user_profile.js
@@ -1,10 +1,23 @@
 const user = require('../models/users');
 
 module.exports.users = function(req, res){
-    return res.render('user', {
-        title: 'codeial'
+    console.log('cookie is ', req.cookies.user_id);
+    user.findOne({_id : req.cookies.user_id},function(err, user1){
+        if(err){console.log('error in getting user');}
+        if(!user1)
+        {
+            console.log('user is not signed in');
+            return res.redirect('/users/sign-in');                  // always use if and else statements else it will  
+        }                                                           //asynchronously call another res.render(if not written under else)
+        else
+        {
+            return res.render('user', {
+                title: 'codeial',
+                name: user1.name
+            });
+        }
     });
-}
+};
 
 module.exports.sign_up = function(req, res){
     return res.render('sign_up',{
@@ -16,16 +29,16 @@ module.exports.sign_in = function(req, res){
     return res.render('sign_in',{
         title: 'codeial / sign_in'
     });
-}
+};
+module.exports.kill_cookie = function(req, res){
+    res.clearCookie('user_id');
+    res.redirect('back');
+};
 
 module.exports.create = function(req, res){
-    console.log("req reached the controller");
     if(req.body.password != req.body.confirm_password)
-        return res.redirect('back');
-    console.log("both password matches");
     console.log(req.body.email);
     user.findOne({email: req.body.email}, function(err, user1){
-        console.log("user searched", user);
         if(err){console.log('error in finding user'); return;}
         if(!user1)
         {
@@ -43,5 +56,16 @@ module.exports.create = function(req, res){
 };
 
 module.exports.create_session = function(req, res){
-    // to do
+    
+    user.findOne({'email': req.body.email, 'password': req.body.password}, function(err, user2){
+        console.log('user2 is_ ', user2);
+        if(!user2)
+            return res.redirect('back');
+        else
+        {
+            console.log('hello', user2);
+            res.cookie('user_id', user2.id);
+            return res.redirect('/');
+        }
+    });
 };
--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const user = require("../controller/user_profile");
 
-router.get('/', user.users);
+router.get('/profile', user.users);
 router.get('/sign-up',user.sign_up);
 router.get('/sign-in', user.sign_in);
-
+router.get('/kill_cookie',user.kill_cookie);
 router.post('/create', user.create);
+router.post('/create_session', user.create_session);
 module.exports = router;
